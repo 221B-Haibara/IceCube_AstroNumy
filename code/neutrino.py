ad6c351c3288b18fe6c9ef8f09043fbbfb259246
221B-Haibara <atomythlu@gmail.com>
2017-10-15 13:31:53 +0900

    neutrino random generation

---
+++ code/neutrino.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from kde import gaussian_kde
+import matplotlib.pyplot as plt
+class NuDist:
+    def __init__(self, ehefile="../Resources/EHE/EHE_effective.csv"):
+        passSR = pd.read_csv(ehefile)
+        bins = np.linspace(-1, 1, 180)
+        m = plt.hist(passSR["cos(ImpLF_zen)"], weights=np.array(passSR["wE3"]) / 0.457845099495, bins=bins, normed=True)
+
+        self.density_nocos = gaussian_kde(np.array(passSR["cos(ImpLF_zen)"]), weights=np.array(passSR["wE3"]))
+        self.density_nocos.set_bandwidth(0.04)
+        self.x_nocos = m[1]
+
+        bins = np.linspace(0, 180, 180)
+        m = plt.hist(np.rad2deg(np.arccos(passSR["cos(ImpLF_zen)"])), weights=np.array(passSR["wE3"]) / 0.457845099495,
+                     bins=bins, normed=True)
+
+        self.density = gaussian_kde(np.rad2deg(np.arccos(np.array(passSR["cos(ImpLF_zen)"]))),
+                               weights=np.array(passSR["wE3"]))
+        self.density.set_bandwidth(0.05)
+        self.x = m[1]
+
+
+    def show_fits(self):
+        plt.figure()
+        plt.plot(self.x_nocos, self.density_nocos(self.x_nocos), color="red", label="kde(bright)")
+        plt.show()
+
+        plt.figure()
+        plt.plot(self.x, self.density(self.x), color="red", label="kde(bright)")
+        plt.show()
+
+    def get_random(self, size):
+        """
+        Draw random neutrino
+        :param size: how many neutrinos
+        :return: [(declination, right_ascension), (...),  ...]
+        """
+        theta = self.zenith2declination(self.density.resample(size=size)[0])
+        phi = np.random.uniform(0,2*np.pi,size=size)
+        print len(theta)
+        print len(phi)
+        data = np.array([theta,phi])
+        return data.T
+
+    @staticmethod
+    def zenith2declination(theta):
+        return theta - 90.
+
+if __name__ == "__main__":
+    nudist = NuDist()
+    print nudist.get_random(1)
+
